4393c0af17a563887910dd811aa9810f17da8fdf
tlaze <tomlazore@gmail.com>
2023-05-03 14:29:30 -0400

    Route to signup user works

--- src/main/java/revature/com/eCommerce/Service/EcommerceService.java
+++ src/main/java/revature/com/eCommerce/Service/EcommerceService.java
@@ -1,12 +1,19 @@
 package revature.com.eCommerce.Service;
 
 import org.springframework.stereotype.Service;
+import revature.com.eCommerce.ECommerceApplication;
 import revature.com.eCommerce.Model.EcommerceUser;
 
 @Service
 public class EcommerceService {
 
+    EcommerceInterface ecommerceInterface;
+
+    public EcommerceService(EcommerceInterface ecommerceInterface) {
+        this.ecommerceInterface = ecommerceInterface;
+    }
+
     public EcommerceUser signup(EcommerceUser user){
-        return EcommerceInterface.save(user);
+        return ecommerceInterface.save(user);
     }
 }
